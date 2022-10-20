cc549dffbb1b1ca63b29f22e87f5920fdb4edfea
傅鑫豪 <79181357+fuxinhao@users.noreply.github.com>
2022-10-21 00:09:29 +0800

    Update GitTest.java

--- src/main/java/com/pet/demo/GitTest.java
+++ src/main/java/com/pet/demo/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("测试一下上传代码到github");
         System.out.println("测试一下ssh方式上传代码到github");
+        System.out.println("测试拉取代码pull");
     }
 }
