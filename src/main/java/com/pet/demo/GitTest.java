e5d7cb280e4e0d34062ddc3c9acfc8e9f60eeb1e
FuXinHao <2434073642@qq.com>
2022-10-20 23:57:07 +0800

    添加git测试类第二次

--- src/main/java/com/pet/demo/GitTest.java
+++ src/main/java/com/pet/demo/GitTest.java
@@ -3,6 +3,6 @@ package com.pet.demo;
 public class GitTest {
 
     public static void main(String[] args) {
-        System.out.println("测试一下git");
+        System.out.println("测试一下上传代码到github");
     }
 }
